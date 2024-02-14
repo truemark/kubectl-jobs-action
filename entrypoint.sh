3486bc5f5bab3b790361c3e3d6d095aec0585e21
Louie Celli <v-lcelli@youngliving.com>
2024-02-14 09:06:41 -0700

    Removing the functions and running just loops.

--- entrypoint.sh
+++ entrypoint.sh
@@ -69,11 +69,48 @@ else
     echo "NAMESPACE is not set. Exiting..."
     exit 1
   fi
-  jobStatusOutput=$(job_status)
-  jobLogsOutput=$(job_logs)
-  output="${jobStatusOutput}\n${jobLogsOutput}"
-fi
+#  jobStatusOutput=$(job_status)
+#  jobLogsOutput=$(job_logs)
+#  output="${jobStatusOutput}\n${jobLogsOutput}"
+#fi
 
+
+  while true; do
+
+    POD_NAME=$(kubectl get pods -n "$NAMESPACE" | grep $JOB_NAME | awk '{print $1}');
+    POD_STATUS=$(kubectl get pod "$POD_NAME" -n "$NAMESPACE" -o jsonpath='{.status.phase}')
+
+    if [ "$POD_STATUS" == "Running" ]; then
+        response+="Pod $POD_NAME is running. Following logs for $LOG_FOLLOW_DURATION seconds..."
+        # Follow the logs for a specified duration
+        log_output=$(timeout $LOG_FOLLOW_DURATION kubectl logs -f "$POD_NAME" -n "$NAMESPACE")
+        response+="$log_output\n"
+        break # Exit the loop
+    elif [ "$POD_STATUS" == "Error" ]; then
+        kubectl delete pod $POD_NAME -n "$NAMESPACE"
+    else
+        response+="Pod $POD_NAME is not in 'Running' status. Current status is $POD_STATUS. Retrying in $SLEEP_TIME seconds..."
+        sleep $SLEEP_TIME
+    fi
+  done
+
+  while true; do
+      STATUS=$(kubectl get job $JOB_NAME -n $NAMESPACE -o jsonpath='{.status.conditions[?(@.type=="Complete")].status}')
+
+      if [ "$STATUS" == "True" ]; then
+          response+="\nJob: $JOB_NAME completed successfully\n"
+          sleep 1
+          kubectl delete -f $JOB_FILEPATH -n "$NAMESPACE"
+          exit 0
+      elif [ "$STATUS" == "False" ]; then
+          response+="Job failed"
+      else
+          response+="Job is still running"
+          sleep $SLEEP_TIME
+      fi
+  done
+
+fi
 {
   echo "response<<EOF";
   echo "$output";
