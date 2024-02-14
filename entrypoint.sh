f7485fade133b0c7b60714747af287659d6c7312
Louie Celli <v-lcelli@youngliving.com>
2024-02-14 14:04:37 -0700

    Adding in a counter for the initial loop.

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,18 @@ else
     exit 1
   fi
 
+  COUNTER=0
+  MAX_RETRIES=15
+
   while true; do
+    # Increment counter
+    COUNTER=$((COUNTER+1))
+
+    # Check if counter has reached the max retries
+    if [ "$COUNTER" -ge "$MAX_RETRIES" ]; then
+        echo "Maximum retries ($MAX_RETRIES) reached. Exiting..."
+        break
+    fi
 
     POD_NAME=$(kubectl get pods -n "$NAMESPACE" | grep $JOB_NAME | awk '{print $1}');
     POD_STATUS=$(kubectl get pod "$POD_NAME" -n "$NAMESPACE" -o jsonpath='{.status.phase}')
@@ -46,7 +57,7 @@ else
       STATUS=$(kubectl get job $JOB_NAME -n $NAMESPACE -o jsonpath='{.status.conditions[?(@.type=="Complete")].status}')
 
       if [ "$STATUS" == "True" ]; then
-          echo "\nJob: $JOB_NAME completed successfully\n"
+          echo -e "\nJob: $JOB_NAME completed successfully\n"
           sleep 1
           kubectl delete -f $JOB_FILEPATH -n "$NAMESPACE"
           exit 0
